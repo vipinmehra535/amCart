c37bf2d84b3dfb054e4fb563f6f1c0171fc795c2
vipinmehra535 <vipinsinghmehra535@gmail.com>
2024-07-31 23:45:12 +0530

    started working on admin js Route

--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:amcart/constants/utlis.dart';
+import 'package:amcart/models/product.dart';
+import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:flutter/material.dart';
 
 class AdminServices {
@@ -13,7 +15,27 @@ class AdminServices {
     required String category,
     required List<File> images,
   }) async {
-    try {} catch (e) {
+    try {
+      final cloudinary = CloudinaryPublic('dwqjuckio', 'mvk6zkky');
+      List<String> imageUrls = [];
+
+      for (int i = 0; i < images.length; i++) {
+        CloudinaryResponse res = await cloudinary.uploadFile(
+          CloudinaryFile.fromFile(images[i].path, folder: name),
+        );
+        imageUrls.add(res.secureUrl);
+
+        Product product = Product(
+          name: name,
+          description: description,
+          quantity: quantity,
+          images: imageUrls,
+          category: category,
+          price: price,
+        );
+      }
+    } catch (e) {
+      if (!context.mounted) return;
       showSnackBar(context, e.toString());
     }
   }
