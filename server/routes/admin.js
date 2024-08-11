53fd2b8b416551d7c8084187c58c2e02405fe8f2
vipinmehra535 <vipinsinghmehra535@gmail.com>
2024-08-11 21:21:31 +0530

    working add product route

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,4 +1,16 @@
 const express = require("express");
-
 const adminRouter = express.Router();
+const admin = require("../middleware/admin");
 
+// Add Products
+
+adminRouter.get("/admin/add-product", admin, async (req, res) => {
+  try {
+    const { name, description, images, category, quantity, price } = req.body;
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = adminRouter;
