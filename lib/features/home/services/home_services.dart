f072d23596b24b86614ccf81596beeb92ec5836d
vipinmehra535 <vipinsinghmehra535@gmail.com>
2024-09-03 23:15:07 +0530

    fetching category based product

---
+++ lib/features/home/services/home_services.dart
@@ -0,0 +1,9 @@
+import 'package:amcart/models/product.dart';
+import 'package:flutter/material.dart';
+
+class HomeServices {
+  Future<List<Product>> fetchCategoryProduct({
+    required BuildContext context,
+    required String category,
+  }) {}
+}
